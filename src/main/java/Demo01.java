7ca58c6e2b2ee143b4a0d89f524cbadd009e0936
yisheng <ys@yisheng.cn>
2020-07-30 11:53:07 +0800

    提交

---
+++ src/main/java/Demo01.java
@@ -0,0 +1,11 @@
+/**
+ * @author yisheng
+ * @description :
+ * @Date 2020/7/30 11:48
+ * @version: 1.0
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("Demo01");
+    }
+}
