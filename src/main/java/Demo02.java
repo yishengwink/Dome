e8b4da24c1388eff4822cd055498b4e005d7fbea
yisheng <ys@yisheng.cn>
2020-07-30 13:12:31 +0800

    编写Demo02,完成输出

---
+++ src/main/java/Demo02.java
@@ -0,0 +1,17 @@
+import sun.java2d.pipe.SpanIterator;
+
+import java.util.HashMap;
+
+/**
+ * @author yisheng
+ * @description :
+ * @Date 2020/7/30 13:09
+ * @version: 1.0
+ */
+public class Demo02 {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("yisheng","wink");
+        System.out.println(map.get("yisheng"));
+    }
+}
